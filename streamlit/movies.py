febbc6397536891ef700456c4b048c86ce4d6565
Emartinez08 <A01552626@itesm.mx>
2025-03-01 13:03:38 -0600

    m

---
+++ streamlit/movies.py
@@ -0,0 +1,9 @@
+#leer el archivo /home/kike/Documents/BIG_DATA/csv/movies.csv
+
+import pandas as pd
+import streamlit as st
+
+# Open and read the CSV file
+file_path = '/home/kike/Documents/BIG_DATA/csv/movies.csv'
+movie_data = pd.read_csv(file_path)
+
